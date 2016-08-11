7aa6dfb6ec929132551982450f52a541782cb442
nguyendac8 <congdacit@gmail.com>
2016-08-11 16:05:53 +0700

    Validation!

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160811084709) do
+ActiveRecord::Schema.define(version: 20160811085834) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.text     "description", limit: 65535
+    t.string   "image_url",   limit: 255
+    t.decimal  "price",                     precision: 8, scale: 2
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
